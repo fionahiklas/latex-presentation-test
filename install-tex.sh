5f7216250ad7ccb5598d938047bb8a8c3045a0ec
Fiona Bianchi <fiona@hiklas.co.uk>
2021-08-03 22:32:37 +0100

    Fixed a typo

--- install-tex.sh
+++ install-tex.sh
@@ -4,7 +4,7 @@
 wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz 
 tar -zxf install-tl-unx.tar.gz
 cd install-tl-20*
-./intall-tl --profile /vagrant/texlive.profile
+./install-tl --profile /vagrant/texlive.profile
 
 source /vagrant/setup-user-packages.sh
 
